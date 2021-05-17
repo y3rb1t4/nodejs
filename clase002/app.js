09049b95726835e51cba9aefbdb5104a20b346c9
Y3rbit4 <hrodriguez@frba.utn.edu.ar>
2021-05-17 19:58:05 -0300

    clase 002 actualizacion

--- clase002/app.js
+++ clase002/app.js
@@ -1,34 +1,32 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
 //const indexRouter = require('./routes/index');
 //const usersRouter = require('./routes/users');
-
+const products = require("./routes/products");
 const app = express();
 
-
-
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
+app.use("/products", products);
 //app.use('/', indexRouter);
 //app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   // next(createError(404));
   res.sendStatus(404); // 404
-
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   console.error(err); // log forever
   res.sendStatus(500);
--- clase002/routes/products.js
+++ clase002/routes/products.js
@@ -1,6 +1,16 @@
 const { Router } = require("express");
 const router = Router();
 
+const { all, create, find } = require("../controllers/products");
+
+const { validateCreate } = require("./../middlewares/actions/product");
+
 // router -> GET , POST , PUT (actualizacion completa ) , DELETE , PATCH (actualizacion parcial)
+//localhost:8000/products/find/1 [GET]
+router.get("/:id", find);
+//localhost:8000/products [GET]
+router.get("/", all);
+//localhost:8000/products [POST]
+router.post("/", create);
 
 module.exports = router;
